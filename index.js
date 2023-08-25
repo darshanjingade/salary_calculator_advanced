eada40bd4e2e35af70ba891466f1bef7f38dfffe
darshanjingade <84464911+darshanjingade@users.noreply.github.com>
2023-08-25 17:31:29 +0530

    adding the source code
    need the functionality improvement

---
+++ index.js
@@ -0,0 +1,15 @@
+var app=angular.module("myApp",[]);
+    app.controller("myCont",function($scope){
+        $scope.CTC
+        $scope.ret=false 
+        $scope.add1="Type of Deduction"    
+        $scope.add2="Type of Deduction"
+        $scope.pfemp=0    
+        $scope.pfempr=0  
+        $scope.pftax=0
+        $scope.insu=0
+        $scope.additionalValue1=0
+        $scope.additionalValue2=0
+
+
+    })
